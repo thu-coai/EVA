79c10ff36071a0e4a302fb4dbb181b8f6af91e3c
gyxbndsthu <guyx21@mails.tsinghua.edu.cn>
2022-01-20 02:07:04 +0000

    generation gc test

---
+++ src/scripts/eva_inference_static_gc_test.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+WORKING_DIR=/home/guyuxian/eva-origin/
+
+# Change for multinode config
+MP_SIZE=1
+
+NUM_WORKERS=1
+NUM_GPUS_PER_WORKER=8
+
+DATA_PATH="/dataset/f1d6ea5b/gyx/data/kdconv"
+
+CONFIG_PATH="${WORKING_DIR}/src/configs/model/eva2.0_model_config.json"
+CKPT_PATH="${WORKING_DIR}/checkpoints/eva2.0"
+
+LR=${2-0.001}
+WM=${3-0.01}
+GRAD_ACC=${4-1}
+
+SAVE_PATH="${WORKING_DIR}/results/test_eva_infer_finetune/"
+# SAVE_PATH="${WORKING_DIR}/results/test/"
+LOG_FILE="${SAVE_PATH}/log.txt"
+DS_CONFIG="${WORKING_DIR}/src/configs/deepspeed/eva_ds_config.json"
+TOKENIZER_PATH="${WORKING_DIR}/bpe_dialog_new"
+
+BATCH_SIZE=32
+TRAIN_ITER=-1
+EPOCHS=3
+
+ENC_LEN=128
+DEC_LEN=128
+
+
+OPTS=""
+OPTS+=" --model-config ${CONFIG_PATH}"
+OPTS+=" --model-parallel-size ${MP_SIZE}"
+OPTS+=" --eval-batch-size ${BATCH_SIZE}"
+OPTS+=" --enc-seq-length ${ENC_LEN}"
+OPTS+=" --dec-seq-length ${DEC_LEN}"
+OPTS+=" --save ${SAVE_PATH}"
+OPTS+=" --log-file ${LOG_FILE}"
+OPTS+=" --load ${CKPT_PATH}"
+OPTS+=" --data-path ${DATA_PATH}"
+OPTS+=" --distributed-backend nccl"
+OPTS+=" --lr ${LR}"
+OPTS+=" --warmup ${WM}"
+OPTS+=" --tokenizer-path ${TOKENIZER_PATH}"
+OPTS+=" --checkpoint-activations"
+OPTS+=" --deepspeed-activation-checkpointing"
+OPTS+=" --fp16"
+OPTS+=" --deepspeed"
+OPTS+=" --deepspeed_config ${DS_CONFIG}"
+OPTS+=" --do-eval"
+OPTS+=" --eval-generation"
+
+CMD="python3 -m torch.distributed.launch --master_port 1234 --nproc_per_node ${NUM_GPUS_PER_WORKER} ${WORKING_DIR}/src/eva_finetune.py ${OPTS}"
+
+echo ${CMD}
+mkdir -p ${SAVE_PATH}
+${CMD} 2>&1 | tee ${SAVE_PATH}/train_log
